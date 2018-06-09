e596abe26f25c7a276cf4f11cdb35f5cc451e372
Cenk Tura <cenk.tura@softtech.com.tr>
2018-06-09 20:46:05 +0300

    test

---
+++ Sample/src/java8/lambda/Deneme.java
@@ -0,0 +1,12 @@
+package java8.lambda;
+
+import java.io.File;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+		String[] pathElements = ("POSMATIK_UK0196" + File.separator + "F215dfg").split(File.separator);
+
+	}
+
+}
